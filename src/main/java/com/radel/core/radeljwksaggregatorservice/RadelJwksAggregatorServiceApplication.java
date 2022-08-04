46147869137107d1e1cb89f40aebdcfe5254ac7c
Paweł Radel <pawel.radel@nvt.pl>
2022-08-04 15:11:20 +0200

    first commit

---
+++ src/main/java/com/radel/core/radeljwksaggregatorservice/RadelJwksAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.radel.core.radeljwksaggregatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadelJwksAggregatorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadelJwksAggregatorServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/radel/core/radeljwksaggregatorservice/RadelJwksAggregatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radel.core.radeljwksaggregatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadelJwksAggregatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
